46317ae070678d803d386c1320bdb088157564d7
Monicat10 <86111099+Monicat10@users.noreply.github.com>
2021-06-18 21:47:18 +0530

    Create CountryService.java

---
+++ orm-learn/src/main/java/com/cognizant/ormlearn/service/CountryService.java
@@ -0,0 +1,11 @@
+package com.cognizant.ormlearn.service;
+
+import com.cognizant.ormlearn.model.Country;
+
+import java.util.List;
+
+public interface CountryService {
+
+	public List<Country> getAllCountries();
+
+}
