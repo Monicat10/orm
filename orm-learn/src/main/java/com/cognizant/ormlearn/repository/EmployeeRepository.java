114cd448b5aa5719ffb14f381b94922e096ff059
Monicat10 <86111099+Monicat10@users.noreply.github.com>
2021-06-29 14:37:00 +0530

    Update EmployeeRepository.java

--- orm-learn/src/main/java/com/cognizant/ormlearn/repository/EmployeeRepository.java
+++ orm-learn/src/main/java/com/cognizant/ormlearn/repository/EmployeeRepository.java
@@ -11,10 +11,13 @@ import com.cognizant.ormlearn.model.Employee;
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 	@Query(value = "SELECT e FROM Employee e left join fetch e.department d left join fetch e.skillList WHERE e.permanent = 1")
 	List<Employee> getAllPermanentEmployees();
-
-	@Query(value = "SELECT AVG(e.salary) FROM Employee e where e.department.id = :id")
+	
+	@Query("select avg(e.salary) from Employee e ")
+	double getAverageSalary();
+	
+	@Query(value="SELECT AVG(e.salary) FROM Employee e where e.department.id = :id")
 	double getAverageSalaryBasedOnDeptId(@Param("id") int id);
-
-	@Query(value = "SELECT * FROM employee", nativeQuery = true)
-	List<Employee> getAllEmployeesNativeQuery();
-}
+	
+	//Avoiding native queries helps in easier portability of databases
+	@Query(value="SELECT * FROM employee", nativeQuery = true)
+	List<Employee> getAllEmployeesNative();}
