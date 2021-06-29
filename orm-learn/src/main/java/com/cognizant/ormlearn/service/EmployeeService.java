6bbaa5ed1b76f7e3d271ec19cbeb696877065b04
Monicat10 <86111099+Monicat10@users.noreply.github.com>
2021-06-29 11:25:51 +0530

    Update EmployeeService.java

--- orm-learn/src/main/java/com/cognizant/ormlearn/service/EmployeeService.java
+++ orm-learn/src/main/java/com/cognizant/ormlearn/service/EmployeeService.java
@@ -5,18 +5,14 @@ import java.util.List;
 import com.cognizant.ormlearn.model.Employee;
 
 public interface EmployeeService {
-
-	Employee findEmployee(int id);
 	
-	void removeEmployee(int id);
+	Employee findEmployee(int id);
 
 	void saveEmployee(Employee employee);
-	
+
 	List<Employee> findAllPermanentEmployees();
-	
-	double findAverageSalaryofEmployees();
-	
+
 	double findAverageSalaryBasedOnDeptId(int id);
-	
-	List<Employee> getAllEmployeesUsingNativeQuery(); 
+
+	List<Employee> getAllEmployeesUsingNativeQuery();
 }
