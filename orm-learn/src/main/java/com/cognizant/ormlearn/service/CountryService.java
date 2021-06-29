31df3931eb3dd62889e12915f4494d3a52cf73fd
Monicat10 <86111099+Monicat10@users.noreply.github.com>
2021-06-29 10:53:42 +0530

    Update CountryService.java

--- orm-learn/src/main/java/com/cognizant/ormlearn/service/CountryService.java
+++ orm-learn/src/main/java/com/cognizant/ormlearn/service/CountryService.java
@@ -1,11 +1,28 @@
 package com.cognizant.ormlearn.service;
 
-import com.cognizant.ormlearn.model.Country;
-
 import java.util.List;
+
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.service.exp.CountryNotFoundException;
 
 public interface CountryService {
 
 	public List<Country> getAllCountries();
 
+	public void addCountry(Country country);
+
+	public void deleteCountry(String code);
+	
+	public void updateCountry(String code, String name);
+	
+	public List<Country> SortCountry(String name);
+	
+	public List<Country> SearchCountry(String name);
+
+	public Country findCountryCode(String countryCode) throws CountryNotFoundException;
+
+	
+
+
+	
 }
